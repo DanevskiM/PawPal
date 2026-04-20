9a953771664da84554b5bfd3f58f267311fa9882
DanevskiM <danevskimetodija5@gmail.com>
2026-04-20 12:54:22 +0200

    first-commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Header } from './header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  imports: [Header],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
 })
 export class App {
   protected readonly title = signal('PawPal');
