5ddb50381243a94b910ec988c5a5b0f1046a2afb
DanevskiM <danevskimetodija5@gmail.com>
2026-04-20 20:53:03 +0200

    small fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header';
-import { Formular } from './formular/formular.component';
-import { ReviewsComponent } from './reviews/reviews.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, Formular, ReviewsComponent, RouterOutlet],
+  imports: [HeaderComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
