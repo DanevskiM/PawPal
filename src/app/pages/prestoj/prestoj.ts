cf8e10bcab36cd3b42b21d3051aa391d2ece32e2
Marko <markokadzbanov@Markos-MacBook-Air.local>
2026-04-20 13:28:35 +0200

    add pages components and routes

---
+++ src/app/pages/prestoj/prestoj.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-prestoj',
+  imports: [],
+  templateUrl: './prestoj.html',
+  styleUrl: './prestoj.css',
+})
+export class Prestoj {}
---
+++ src/app/pages/za-nas/za-nas.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-za-nas',
+  imports: [],
+  templateUrl: './za-nas.html',
+  styleUrl: './za-nas.css',
+})
+export class ZaNas {}
