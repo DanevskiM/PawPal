80880df7251367d35f280ccf141585110db4777a
DanevskiM <danevskimetodija5@gmail.com>
2026-04-20 14:09:53 +0200

    review-commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { Header } from './header/header';
 import { Formular } from './formular/formular.component';
+import { Reviews } from './reviews/reviews.component';
 
 @Component({
   selector: 'app-root',
-  imports: [Header, Formular],
+  imports: [Header, Formular, Reviews],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
