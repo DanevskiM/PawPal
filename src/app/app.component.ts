88a260f00f6b2de8fc2328669eba5eafc0165311
DanevskiM <danevskimetodija5@gmail.com>
2026-04-20 13:23:44 +0200

    second-commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { Header } from './header/header';
+import { Formular } from './formular/formular.component';
 
 @Component({
   selector: 'app-root',
-  imports: [Header],
+  imports: [Header, Formular],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
---
+++ src/app/formular/formular.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-formular',
+  imports: [],
+  templateUrl: './formular.component.html',
+  styleUrl: './formular.component.css',
+})
+export class Formular {}
