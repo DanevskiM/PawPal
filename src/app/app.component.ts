1dfe9ce1b8019bb98899481a46cbe45cea89d5ab
DanevskiM <danevskimetodija5@gmail.com>
2026-04-20 20:46:11 +0200

    fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header';
+import { Formular } from './formular/formular.component';
+import { ReviewsComponent } from './reviews/reviews.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [HeaderComponent, Formular, ReviewsComponent, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'PawPal';
--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { Formular } from '../../formular/formular.component';
+import { ReviewsComponent } from '../../reviews/reviews.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [Formular, ReviewsComponent],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
