426eb335c5c1dfc500e8cab0d297b2399210af48
Marko <markokadzbanov@Markos-MacBook-Air.local>
2026-04-24 02:43:43 +0200

    footer

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header';
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, RouterOutlet],
+  imports: [HeaderComponent, RouterOutlet, Footer],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
